9d1c6465386bc0bbf35874c994d9afce2e9e5e9e
Jaya <jayababushrestha@gmail.com>
2024-03-24 09:40:46 +1100

    second commit

--- mindmap_toolkit.py
+++ mindmap_toolkit.py
@@ -1,7 +1,7 @@
 from abc import ABC
 from superagi.tools.base_tool import BaseToolkit, BaseTool
 from typing import Type, List
-from Mindmap_tool import MindmapTool
+from mindmap_tool import MindmapTool
 
 class MindmapToolkit(BaseToolkit, ABC):
     name: str = "Mindmap Toolkit"
